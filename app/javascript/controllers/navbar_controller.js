4439691cdf70044e6eba8e9f419fbdf19e44db8b
Andi Messingschlager <andi.messings@gmail.com>
2025-10-25 12:55:01 +0200

    Click on mini-search-bar works now

--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -1,53 +1,71 @@
 import { Controller } from "@hotwired/stimulus"
 
-// Connects to data-controller="navbar"
 export default class extends Controller {
   connect() {
     this.navbar = this.element
     this.miniSearch = this.navbar.querySelector(".mini-search-bar")
+    this.isExpanded = false
 
-    // Bind methods so they can be added/removed as listeners safely
     this.handleScroll = this.handleScroll.bind(this)
-    this.handleClickOutside = this.handleClickOutside.bind(this)
+    this.handleDocClick = this.handleDocClick.bind(this)
+    this.handleKeydown = this.handleKeydown.bind(this)
     this.expandMiniSearch = this.expandMiniSearch.bind(this)
+    this.collapse = this.collapse.bind(this)
 
-    // Initial check + listeners
     this.handleScroll()
     window.addEventListener("scroll", this.handleScroll)
     window.addEventListener("load", this.handleScroll)
-    document.addEventListener("click", this.handleClickOutside)
+    document.addEventListener("click", this.handleDocClick, true)
+    document.addEventListener("keydown", this.handleKeydown)
 
     if (this.miniSearch) {
-      this.miniSearch.addEventListener("click", this.expandMiniSearch)
+      this.miniSearch.addEventListener("pointerdown", this.expandMiniSearch, { passive: false })
     }
   }
 
   disconnect() {
-    // Clean up listeners when Turbo navigates
     window.removeEventListener("scroll", this.handleScroll)
     window.removeEventListener("load", this.handleScroll)
-    document.removeEventListener("click", this.handleClickOutside)
+    document.removeEventListener("click", this.handleDocClick, true)
+    document.removeEventListener("keydown", this.handleKeydown)
 
     if (this.miniSearch) {
-      this.miniSearch.removeEventListener("click", this.expandMiniSearch)
+      this.miniSearch.removeEventListener("pointerdown", this.expandMiniSearch)
     }
   }
 
   handleScroll() {
     if (window.scrollY > 100) {
-      this.navbar.classList.add("scrolled")
+      if (!this.isExpanded) this.navbar.classList.add("scrolled")
     } else {
       this.navbar.classList.remove("scrolled")
     }
   }
 
-  handleClickOutside(e) {
-    if (!this.navbar.contains(e.target) && window.scrollY > 100) {
-      this.navbar.classList.add("scrolled")
-    }
+  handleDocClick(e) {
+    if (this.navbar.contains(e.target)) return
+    this.collapse()
   }
 
-  expandMiniSearch() {
+  handleKeydown(e) {
+    if (e.key === "Escape") this.collapse()
+  }
+
+  expandMiniSearch(e) {
+    e.preventDefault()
+    e.stopPropagation()
+    this.isExpanded = true
+    this.navbar.classList.add("expanded")
     this.navbar.classList.remove("scrolled")
+    requestAnimationFrame(() => {
+      const firstInput = this.navbar.querySelector(".search-bar input")
+      if (firstInput) firstInput.focus()
+    })
+  }
+
+  collapse() {
+    this.isExpanded = false
+    this.navbar.classList.remove("expanded")
+    if (window.scrollY > 100) this.navbar.classList.add("scrolled")
   }
 }
